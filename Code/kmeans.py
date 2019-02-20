4bef120373992661d8a8a1bf07bd468232951937
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-02-20 01:19:15 -0800

    #2 Jiawei made some changes to kmeans.py

--- Code/kmeans.py
+++ Code/kmeans.py
@@ -16,8 +16,8 @@ def show_img(img):
     plt.title('Image')
     plt.show()
 
-### to grayscale
-def rgb2gray(rgb):
+### greyscale
+def greyscale(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 ### read
@@ -27,11 +27,11 @@ def read_img(path):
         i += 1
         print(filename)
         hand = mpimg.imread(path + filename)
-        grayscale = rgb2gray(hand)
+        hand = greyscale(hand)
         if i == 1:
-            set = np.array(grayscale)
+            set = np.array(hand)
         else:  
-            set = np.concatenate( ( set, np.array(grayscale) ), axis = 0 )
+            set = np.concatenate( ( set, np.array(hand) ), axis = 0 )
 
     
     set = set.reshape( i, 480, 640)
@@ -39,31 +39,53 @@ def read_img(path):
 
     return set
 
+
 ### main
 png = read_img("png\\")
 jpeg = read_img("jpeg\\")
+print("--------------------------\n")
 
-png_feed = png.reshape(png.shape[0], png.shape[1] * png.shape[2])
-jpeg_feed = jpeg.reshape(jpeg.shape[0], jpeg.shape[1] * jpeg.shape[2])
+png_feed = png.reshape(png.shape[0] * png.shape[1] * png.shape[2], 1) * 255.
+jpeg_feed = jpeg.reshape(jpeg.shape[0] * jpeg.shape[1] * jpeg.shape[2], 1)
 
-n_clusters = 3
-kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-# png
-clusters = kmeans.fit_predict(png_feed)
-clusters = np.array(clusters)
-clusters = clusters.reshape(clusters.shape[0], 1)
 
-# jpeg
-clusters_jpeg = kmeans.fit_predict(jpeg_feed)
-clusters_jpeg = np.array(clusters_jpeg)
-clusters_jpeg = clusters_jpeg.reshape(clusters_jpeg.shape[0], 1)
+#for i in range(png_feed.shape[0]):
+#    if png_feed[i] < 16.:
+#        jpeg_feed[i] = 0
 
-# show typical png
-for i in range(n_clusters):
-    print("-----------------------------")
-    print("png clusters\t", i, " = ", clusters[i])
-    print("jpeg clusters\t", i, " = ", clusters_jpeg[i])
 
+# KMEANS
+n_clusters = 2
+kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(jpeg_feed)
+labels = kmeans.labels_
+#for i in range(labels.size):
+#    if png_feed[i] < 16.:
+#        labels[i] = 0
+#kmeans.labels_ = labels
+
+# update labels
+#labels = np.array(labels)
+#labels = labels.reshape(labels.shape[0], 1)
+
+
+
+#masks = (labels == png_feed)
+#print(masks)
+
+
+# read a test
+test = read_img("test\\")
+test = test.reshape(480 * 640, 1)
+
+
+# predict
+result = kmeans.predict(test)
+result = result.reshape(480, 640)
+show_img(result)
+
+
+
+# show typical img
 #fig, ax = plt.subplots(2, n_clusters, figsize=(7, 3))
 #centers = kmeans.cluster_centers_.reshape(n_clusters, 480, 640)
 #for axi, center in zip(ax.flat, centers):
@@ -72,7 +94,7 @@ for i in range(n_clusters):
 #plt.show()
 
 # accuracy
-print( accuracy_score(clusters, clusters_jpeg) )
+#print( accuracy_score(clusters_png, clusters_jpeg) )
 
 #digits = load_digits()
 
