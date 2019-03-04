4838492c1a69c386c60adccb3f28e8e66c71db6c
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-03 23:39:44 -0800

    #4 Created a basic for loop to find two files of the same name.

--- Code/analysis.py
+++ Code/analysis.py
@@ -28,6 +28,13 @@ for kmeansFile in os.listdir(kmeansPath):
 # True negative is when they agree on black.
 # False positive is when kmeans says white when truth says black.
 # False negative is when kmeans says black when truth says white.
+truthPath = "Robot Arm Pictures\\Photoshop Masks"
+kmeansPath = "Robot Arm Pictures\\K-means Strawman"
+
+for truthFile in os.listdir(truthPath):
+    for kmeansFile in os.listdir(kmeansPath):
+        if(truthFile == kmeansFile):
+            print("Found a match with file: " + truthFile)
 
 # All I want to do is print out the integer counter for these four states.
 # I am expecting to manually record them.
