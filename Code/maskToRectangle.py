93708025603088be4919c95cfc8a52dd0c1b3346
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-05 20:40:41 -0800

    #7 Added some basic logic for finding the two points. Honestly, it could probably be reworked to be significantly more efficient.

--- Code/maskToRectangle.py
+++ Code/maskToRectangle.py
@@ -21,6 +21,14 @@ with open('rectangles.csv', 'w', newline='') as csvFile:
         for row in truthImage:
             for truthPixel in row:
                 if(np.array_equal(truthPixel, [1.0, 1.0, 1.0, 1.0])):
+                    if(currentX > x2):
+                        x2 = currentX
+                    elif(currentX < x1):
+                        x1 = currentX
+                    if(currentY > y2):
+                        y2 = currentY
+                    elif(currentY < y1):
+                        y1 = currentY
                 elif(np.array_equal(truthPixel, [0.0, 0.0, 0.0, 1.0]) != True):
                     truthErrors += 1
                 currentY += 1
