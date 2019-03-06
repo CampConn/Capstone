dd78a96dd17668b6c6743cb6ee316c2ad879fef8
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-05 20:41:53 -0800

    #7 Finally, write findings to a .csv

--- Code/maskToRectangle.py
+++ Code/maskToRectangle.py
@@ -42,3 +42,4 @@ with open('rectangles.csv', 'w', newline='') as csvFile:
             print("Truth errors: " + str(truthErrors))
         print('')
 
+        rectangleWriter.writerow([truthFile, x1, y1, x2, y2])
