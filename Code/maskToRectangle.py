508cf08b81975e625c40b9f7dc77740d594cda44
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-05 20:36:53 -0800

    #7 More or less copied this call from the Python CSV documentation.

--- Code/maskToRectangle.py
+++ Code/maskToRectangle.py
@@ -3,3 +3,7 @@ import csv                          # Handle CSV Stuff
 import matplotlib.image as mpimg    # Image reading
 import numpy as np                  # Mandatory array stuff
 
+
+with open('rectangles.csv', 'w', newline='') as csvFile:
+    rectangleWriter = csv.writer(csvFile, delimiter= ',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
