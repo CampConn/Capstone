fa4b3df6e4350b1f1e76d682b4d776d54a34ca21
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-18 13:13:45 -0700

    #9 Made a function to streamline svm class definitions.

--- Code/svm.py
+++ Code/svm.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg    # Image reading
 import numpy as np                  # Mandatory array stuff
 from sklearn import svm
 
-### Ooooh, I finally fucking get how this will work.
+# Oh, I finally understand how this will work.
 # Essentially, X is your training data.
 # Y is your definitions.
 # When you call predict, it takes in data like X and returns an expected Y.
@@ -17,10 +17,27 @@ from sklearn import svm
 # Output files will create .pngs.
 # Stretch goal, use all 21 images to build the neural network (this could be slow AF).
 
-x = [[50, 80, 230], [120, 150, 52]]
-y = [1, 0]
-clf = svm.SVC(gamma='scale')
-clf.fit(x, y)
-print(clf.predict([[60, 120, 80]]))
+# x = [[50, 80, 230], [120, 150, 52]]
+# y = [1, 0]
+# clf = svm.SVC(gamma='scale')
+# clf.fit(x, y)
+# print(clf.predict([[60, 120, 80]]))
 # >>> [0]
+
+def maskPngToBooleanList(mask, x1=0, y1=0, x2=639, y2=479):
+    boolList = []
+    truthErrors = 0
+
+    for y in range(y1, (y2 + 1)):
+        for x in range(x1, (x2 + 1)):
+            if(np.array_equal(mask[y][x], [1.0, 1.0, 1.0, 1.0])):
+                boolList.append(1)
+            elif(np.array_equal(mask[y][x], [0.0, 0.0, 0.0, 1.0])):
+                boolList.append(0)
+            else:
+                truthErrors += 1
+                boolList.append(0)
+                # print("Truth Error Coords | Row: " + str(x) + " | " + "Column: " + str(y))
+
+    return boolList
 
