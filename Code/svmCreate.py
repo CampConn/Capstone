c1de039462540e96cf818c31abb8dc5eeb1b0f35
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-19 12:46:51 -0700

    #9 Kept functions that were already implemented. Added CSV function.

--- Code/svmCreate.py
+++ Code/svmCreate.py
@@ -4,3 +4,43 @@ import matplotlib.image as mpimg    # Image reading
 import numpy as np                  # Special array functionality
 from sklearn import svm             # Support Vector Machine
 from joblib import dump, load       # SVM Model Persistence
+
+def smallestRectangle(rectangleCSVPath):
+    with open(rectangleCSVPath, newline='') as csvFile:
+        rectangleReader = list(csv.reader(
+            csvFile, delimiter=',', quotechar='|'))
+
+        # Possible To Do: Parse through CSV to find best rectangle
+        # Only necessary if the csv changes a lot or is given a large amount of data.
+        # for rectangleRow in rectangleReader:
+        #   parse for smallest x2 - x1 and y2 - y1
+        #   result can't be 0, must actually find a rectangle
+
+        return rectangleReader[20]
+
+# Note, this function can be optimized for nearest neighbor groupings.
+def maskPngToBooleanList(mask, x1=0, y1=0, x2=639, y2=479):
+    boolList = []
+    truthErrors = 0
+
+    for y in range(y1, (y2 + 1)):
+        for x in range(x1, (x2 + 1)):
+            if(np.array_equal(mask[y][x], [1.0, 1.0, 1.0, 1.0])):
+                boolList.append(1)
+            elif(np.array_equal(mask[y][x], [0.0, 0.0, 0.0, 1.0])):
+                boolList.append(0)
+            else:
+                truthErrors += 1
+                boolList.append(0)
+
+    return boolList
+
+def pngToList(mask, x1=0, y1=0, x2=639, y2=479):
+    pixelList = []
+
+    for y in range(y1, (y2 + 1)):
+        for x in range(x1, (x2 + 1)):
+            pixelList.append(mask[y][x])
+
+    return pixelList
+
