086afff786e07f9acbf25884308242f53db4db67
Chase McWhirt <mcwhirtc@oregonstate.edu>
2019-03-28 16:34:17 -0700

    #8 Slightly reworked the for loops and generalized names. Made it into a function as well.

--- Code/analysis.py
+++ Code/analysis.py
@@ -19,53 +19,40 @@ def getRectangles(rectangleCSVPath):
 
         return rectangleReader
 
-fileIncrementor = 1
-
-for truthFile in os.listdir(truthPath):
-    print("Trying file " + str(fileIncrementor) + ": " + truthFile)
-    print("-----------------------------------------------")
-    truthImage = mpimg.imread(truthPath + "\\" + truthFile)
-    kmeansImage = mpimg.imread(kmeansPath + "\\" + truthFile)
-
+def performAnalysisOnImages(truthImage, predictionsImage, resolution=307200, x1=0, y1=0, x2=639, y2=479):
     truePositive = 0
     trueNegative = 0
     falsePositive = 0
     falseNegative = 0
     truthErrors = 0
-    kmeansErrors = 0
+    predictionErrors = 0
     totalPixels = 0
     x = 0
     y = 0
     
-    for row in truthImage:
-        for truthPixel in row:
+    for y in range(y1, (y2 + 1)):
+        for x in range(x1, (x2 + 1)):
             totalPixels += 1
-            if(np.array_equal(truthPixel, [1.0, 1.0, 1.0, 1.0])):
-                if(np.array_equal(truthPixel, kmeansImage[y][x])):
+            if(getPixelValue(truthImage[y][x]) == 1.):
+                if(getPixelValue(predictionsImage[y][x]) == 1.):
                     truePositive += 1
-                elif(np.array_equal(kmeansImage[y][x], [0.0, 0.0, 0.0, 1.0])):
-                    falseNegative +=1
+                elif(getPixelValue(predictionsImage[y][x]) == 0.):
+                    falseNegative += 1
                 else:
-                    kmeansErrors += 1
-            elif(np.array_equal(truthPixel, [0.0, 0.0, 0.0, 1.0])):
-                if(np.array_equal(truthPixel, kmeansImage[y][x])):
+                    predictionErrors += 1
+            elif(getPixelValue(truthImage[y][x]) == 0.):
+                if(getPixelValue(predictionsImage[y][x]) == 0.):
                     trueNegative += 1
-                elif(np.array_equal(kmeansImage[y][x], [1.0, 1.0, 1.0, 1.0])):
-                    falsePositive +=1
+                elif(getPixelValue(predictionsImage[y][x]) == 1.):
+                    falsePositive += 1
                 else:
-                    kmeansErrors += 1
+                    predictionErrors += 1
             else:
                 truthErrors += 1
                 # print("Truth Error Coords | Row: " + str(x) + " | " + "Column: " + str(y))
-            x += 1
-        x = 0
-        y += 1
 
-    print("Total pixels: " + str(totalPixels))
-    print("True positives: " + str(truePositive) + " at " + str(truePositive * 100 / totalPixels))
-    print("True negatives: " + str(trueNegative) + " at " + str(trueNegative * 100 / totalPixels))
-    print("False positives: " + str(falsePositive) + " at " + str(falsePositive * 100 / totalPixels))
-    print("False negatives: " + str(falseNegative) + " at " + str(falseNegative * 100 / totalPixels))
+    printResults(totalPixels, resolution, truePositive, trueNegative, falsePositive, falseNegative, truthErrors, predictionErrors)
+
     if(truthErrors > 0):
         print("Truth errors: " + str(truthErrors))
         # print("Truth errors occur when a pixel in the truth image is not black or white.")
